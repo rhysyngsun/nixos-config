5497b5eb27fa04156b6b1b448f89d3a752e4cc6b
Nathan Levesque <rhysyngsun@gmail.com>
2023-04-25 12:57:22 -0400

    Added scripts to manage tutor IP

--- home/common/dev/python/default.nix
+++ home/common/dev/python/default.nix
@@ -35,6 +35,27 @@ let
   '';
 
   tutor = pkgs.writeShellScriptBin "tutor" "tutor-python tutor-env tutor $@";
+
+  jq = lib.getExe pkgs.jq;
+  tail = "${pkgs.coreutils}/bin/tail";
+  docker = "${pkgs.docker}/bin/docker";
+
+  tutor-ip = pkgs.writeShellScriptBin "tutor-ip" ''
+    CADDY_HOSTNAME=$(${tutor}/bin/tutor local dc ps caddy --format=json | ${tail} -n1 | ${jq} -r '.[].Name')
+    CADDY_IP=$(${docker} inspect $CADDY_HOSTNAME | jq -r '.[].NetworkSettings.Networks.tutor_local_default.IPAddress')
+    echo $CADDY_IP
+  '';
+  tutor-ip-watch = pkgs.writeShellScriptBin "tutor-ip-watch" ''
+    function update-tutor-hosts() {
+      TUTOR_IP=$(${tutor-ip}/bin/tutor-ip)
+      printf "%s\n" "x-tutor-hosts: &tutor-hosts" "  - \"local.overhang.io=$TUTOR_IP\"" > ${config.home.homeDirectory}/open-learning/docker-compose.tutor-hosts.yml
+    }
+    update-tutor-hosts
+    ${docker} events --filter 'event=start' --filter 'type=container' --filter 'image=caddy' | while read event
+    do
+      update-tutor-hosts
+    done
+  '';
 in
 {
   home = {
@@ -42,11 +63,19 @@ in
       tutor-python
       tutor-env
       tutor
+      # utils
+      tutor-ip
+      tutor-ip-watch
     ];
+  };
 
-    sessionVariables = {
-      # tutor should use `docker compose` instead of deprecated `docker-compose`
-      
+  systemd.user = {
+    services.tutor-ip = {
+      Service = {
+        ExecStart = "${tutor-ip-watch}/bin/tutor-ip-watch";
+        Type = "simple";
+      };
+      Install.WantedBy = ["default.target"];
     };
   };
 }
