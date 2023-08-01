4552e7422580560294c7f84defb13043617f1c66
Nathan Levesque <rhysyngsun@gmail.com>
2023-08-01 08:21:18 -0400

    fix(nixos): fixed bluez configuration

--- hosts/morrigan/configuration.nix
+++ hosts/morrigan/configuration.nix
@@ -66,13 +66,19 @@
     jack.enable = true;
     wireplumber.enable = true;
   };
+  # NOTE: these settings override the entire `bluez_monitor.properties` value,
+  # so we need to redefine all the defaults
   environment.etc."wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
 		bluez_monitor.properties = {
 			["bluez5.enable-sbc-xq"] = true,
 			["bluez5.enable-msbc"] = true,
 			["bluez5.enable-hw-volume"] = true,
 			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-		}
+      ["bluez5.codecs"] = "[ sbc sbc_xq aac ]"
+      ["bluez5.hfphsp-backend"] = "native"
+      ["bluez5.default.rate"] = 48000
+      ["bluez5.default.channels"] = 2
+    }
 	'';
 
   hardware.bluetooth.enable = true;
